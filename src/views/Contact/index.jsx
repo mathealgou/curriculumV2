309e29c1172b19e818f2310b3c67902428227eda
Matheus Goulart <mathealgou@gmail.com>
2021-12-02 18:54:28 -0300

    Linkedin and email contacts added.

--- src/views/Contact/index.jsx
+++ src/views/Contact/index.jsx
@@ -2,13 +2,27 @@ import { React } from "react";
 import styles from "./styles.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 
 export function Contact() {
   return (
     <div className={styles.contact}>
       <h1>Contact</h1>
-
-      <FontAwesomeIcon icon={faLinkedin} />
+      <div className={styles.container}>
+        <a
+          href="https://www.linkedin.com/in/matheus-goulart-815661210/"
+          className={styles.link}
+        >
+          <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+          <h3 className={styles.title}>LinkedIn</h3>
+        </a>
+      </div>
+      <div className={styles.container}>
+        <a href="mailto:mathealgou@gmail.com" className={styles.link}>
+          <FontAwesomeIcon icon={faEnvelopeSquare} className={styles.icon} />
+          <h3 className={styles.title}>Email</h3>
+        </a>
+      </div>
     </div>
   );
 }
