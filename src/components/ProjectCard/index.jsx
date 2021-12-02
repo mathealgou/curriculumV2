fa57cd322cfd0641849c342f2d70a3a33e6b613a
Matheus Goulart <mathealgou@gmail.com>
2021-12-02 16:55:46 -0300

    Code cleaning.

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 export function ProjectCard(props) {
-  console.log(props.project);
   return (
     <div className={styles.container}>
       <div className={styles.title}>{props.project.name}</div>
